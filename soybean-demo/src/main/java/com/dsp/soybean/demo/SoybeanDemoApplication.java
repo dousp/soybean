a280d6aa50690d9a9313da2dfde2aa1f8bb8bc97
窦树鹏 <>
2020-08-01 09:09:17 +0800

    Initial commit

---
+++ soybean-demo/src/main/java/com/dsp/soybean/demo/SoybeanDemoApplication.java
@@ -0,0 +1,16 @@
+package com.dsp.soybean.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class SoybeanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoybeanDemoApplication.class, args);
+    }
+
+}
