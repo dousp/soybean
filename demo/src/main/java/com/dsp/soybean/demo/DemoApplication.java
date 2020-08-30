2bcdd2072556314ae5f015db0a9a18acaa2d8bd5
Dsp <douspeng@sina.cn>
2020-08-30 15:14:31 +0800

    配置调整

--- demo/src/main/java/com/dsp/soybean/demo/DemoApplication.java
+++ demo/src/main/java/com/dsp/soybean/demo/DemoApplication.java
@@ -3,14 +3,11 @@ package com.dsp.soybean.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
-public class SoybeanDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SoybeanDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
