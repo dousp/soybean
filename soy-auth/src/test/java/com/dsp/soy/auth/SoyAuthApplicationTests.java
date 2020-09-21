c4b1ef04cd9cb356881eba1b09196bd3b4c747e7
Dsp <douspeng@sina.cn>
2020-09-21 23:08:21 +0800

    升级springboot版本

--- soy-auth/src/test/java/com/dsp/soy/auth/SoyAuthApplicationTests.java
+++ soy-auth/src/test/java/com/dsp/soy/auth/SoyAuthApplicationTests.java
@@ -1,13 +1,9 @@
 package com.dsp.soy.auth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SoyAuthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
