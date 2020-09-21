ebb13abe6f1dc27819341ec0d13a4f9c96f9957d
douspeng <douspeng@sina.cn>
2020-09-21 20:01:21 +0800

    尚未完成

--- soy-auth/src/main/java/com/dsp/soy/auth/conf/OAuth2ClientSecurityConfig.java
+++ soy-auth/src/main/java/com/dsp/soy/auth/conf/OAuth2ClientSecurityConfig.java
@@ -1,44 +1,141 @@
 package com.dsp.soy.auth.conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class OAuth2ClientSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // http.oauth2Client(oauth2 -> oauth2
-        //     .clientRegistrationRepository(this.clientRegistrationRepository())
-        //     .authorizedClientRepository(this.authorizedClientRepository())
-        //     .authorizedClientService(this.authorizedClientService())
-        //     .authorizationCodeGrant(codeGrant -> codeGrant
-        //             .authorizationRequestRepository(this.authorizationRequestRepository())
-        //             .authorizationRequestResolver(this.authorizationRequestResolver())
-        //             .accessTokenResponseClient(this.accessTokenResponseClient())
-        //     )
-        // );
+    public void configure(WebSecurity web) {
+        web.ignoring()
+           .antMatchers("/webjars/**");
+
     }
-    //
-    // @Bean
-    // public OAuth2AuthorizedClientManager authorizedClientManager(
-    //         ClientRegistrationRepository clientRegistrationRepository,
-    //         OAuth2AuthorizedClientRepository authorizedClientRepository) {
-    //
-    //     OAuth2AuthorizedClientProvider authorizedClientProvider =
-    //             OAuth2AuthorizedClientProviderBuilder.builder()
-    //                     .authorizationCode()
-    //                     .refreshToken()
-    //                     .clientCredentials()
-    //                     .password()
-    //                     .build();
-    //
-    //     DefaultOAuth2AuthorizedClientManager authorizedClientManager =
-    //             new DefaultOAuth2AuthorizedClientManager(
-    //                     clientRegistrationRepository, authorizedClientRepository);
-    //     authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-    //
-    //     return authorizedClientManager;
-    // }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults())
+            .oauth2Client()
+            // .oauth2Client(oauth2 -> oauth2
+            //     .clientRegistrationRepository(this.clientRegistrationRepository())
+            //     .authorizedClientRepository(this.authorizedClientRepository())
+            //     .authorizedClientService(this.authorizedClientService())
+            //     .authorizationCodeGrant(codeGrant -> codeGrant
+            //             .authorizationRequestRepository(this.authorizationRequestRepository())
+            //             .authorizationRequestResolver(this.authorizationRequestResolver())
+            //             .accessTokenResponseClient(this.accessTokenResponseClient())
+            //     )
+            // )
+            ;
+    }
+
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken()
+                        .clientCredentials()
+                        .password()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        // Assuming the `username` and `password` are supplied as `HttpServletRequest` parameters,
+        // map the `HttpServletRequest` parameters to `OAuth2AuthorizationContext.getAttributes()`
+        authorizedClientManager.setContextAttributesMapper(contextAttributesMapper());
+        return authorizedClientManager;
+    }
+
+    private Function<OAuth2AuthorizeRequest, Map<String, Object>> contextAttributesMapper() {
+        return authorizeRequest -> {
+            Map<String, Object> contextAttributes = Collections.emptyMap();
+            HttpServletRequest servletRequest = authorizeRequest.getAttribute(HttpServletRequest.class.getName());
+            String username = servletRequest.getParameter(OAuth2ParameterNames.USERNAME);
+            String password = servletRequest.getParameter(OAuth2ParameterNames.PASSWORD);
+            if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
+                contextAttributes = new HashMap<>();
+
+                // `PasswordOAuth2AuthorizedClientProvider` requires both attributes
+                contextAttributes.put(OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username);
+                contextAttributes.put(OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password);
+            }
+            return contextAttributes;
+        };
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return ClientRegistration.withRegistrationId("google")
+                .clientId("google-client-id")
+                .clientSecret("google-client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .scope("openid", "profile", "email", "address", "phone")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .clientName("Google")
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("password")
+                .roles("USER")
+                .build();
+        return  new InMemoryUserDetailsManager(user);
+    }
 }
