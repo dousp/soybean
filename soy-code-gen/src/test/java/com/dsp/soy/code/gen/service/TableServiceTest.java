1156afb35f2ebc5d800f67adc8ebcc4d28012133
douspeng <douspeng@sina.cn>
2020-10-12 14:42:34 +0800

    test

---
+++ soy-code-gen/src/test/java/com/dsp/soy/code/gen/service/TableServiceTest.java
@@ -0,0 +1,26 @@
+package com.dsp.soy.code.gen.service;
+
+import com.dsp.soy.code.gen.entity.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+public class TableServiceTest {
+
+    @Resource
+    TableService tableService;
+
+
+    @Test
+    public void queryAllColumns(){
+        List<Member> memberList = tableService.getAllColumns("tjdb1","rbac_role");
+        memberList.forEach(e-> System.out.println(e.toString()));
+        Assertions.assertEquals(11,memberList.size());
+    }
+
+
+}
